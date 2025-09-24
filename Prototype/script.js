43e1501981e62767b71c9d57bb17c8cc51a0e27f
medalae0005 <elbouazzaoui.malae.solicode@gmail.com>
2025-09-24 12:57:39 +0100

    Prototype... :)

---
+++ Prototype/script.js
@@ -0,0 +1,32 @@
+while (true) {
+    let user = prompt("Enter the code : ");
+        if (user === "C1") {
+            alert("Maquetter une application");
+        } else if (user === "C2") {
+            alert("Réaliser une interface utilisateur web statique et adaptable");
+        } else if (user === "C3") {
+            alert("Développer une interface utilisateur web dynamique");
+        } else if (user === "C4") {
+            alert("Réaliser une interface utilisateur avec une solution de gestion de contenu ou e-commerce");
+        } else if (user === "C5") {
+            alert("Créer une base de données - niveau 1");
+        } else if (user === "C6") {
+            alert("Développer les composants d’accès aux données - niveau 1");
+        } else if (user === "C7") {
+            alert("Développer la partie back-end d’une application web ou web mobile - niveau 1");
+        } else if (user === "C8") {
+            alert("Framwork FrontEnd (React, vueJs...)");
+        } else if (user === "T1") {
+            alert("Comprendre la démarche pédagogique à Solicode");
+        } else if (user === "T2") {
+            alert("Collaboration & posture professionnelle");
+        } else if (user === "T3") {
+            alert("Gérer un projet avec Git & GitHub");
+        } else if (user === "exit") {
+            alert("byeee :)");
+            break;
+        } else {
+            alert("ERROR :(")
+        }
+
+}
